666b8fdfce1b1bc470ab6d81798981bb1dac8e1f
MarcosCabrini Riani_dos_Reis <marcosriani@hotmail.com>
2023-12-10 21:18:40 +0000

    Send verification email and verify user

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -10,21 +10,23 @@ import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import {
-  AuthCredentialValidator,
-  TAuthCredentialValidator,
+  AuthCredentialsValidator,
+  TAuthCredentialsValidator,
 } from '@/lib/validators/account-credentials-validator';
+import { trpc } from '@/trpc/client';
 
 const Page = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<TAuthCredentialValidator>({
-    resolver: zodResolver(AuthCredentialValidator),
+  } = useForm<TAuthCredentialsValidator>({
+    resolver: zodResolver(AuthCredentialsValidator),
   });
 
-  const onSubmit = ({ email, password }: TAuthCredentialValidator) => {
-    // Send data to the server
+  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({});
+  const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
+    mutate({ email, password });
   };
 
   return (
